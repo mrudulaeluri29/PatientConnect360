227b3e265a84ddbc47c5dc5e3f61ec25910bd3bd
Navigna Reddy Gangumalla <142638168+navignareddy@users.noreply.github.com>
2025-11-14 07:41:53 -0700

    Delete Server directory

--- Server/prisma/migrations/20251015182154_add_roles/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'PATIENT', 'CAREGIVER', 'CLINICIAN');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'PATIENT';
